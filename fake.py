81bb65c8fa57f9c9149013b0523d038eef26f920
Gabriela Vasconcelos <gaby@asinpa.com>
2022-09-15 11:24:55 -0500

    created for loop to separate people info

--- fake.py
+++ fake.py
@@ -16,58 +16,58 @@ for x in range(20):
     house = f'{random.randint(10000, 99999)}'
     house_num.append(house)
 
-states = {
-    'AK': 'Alaska',
-    'AL': 'Alabama',
-    'AR': 'Arkansas',
-    'AZ': 'Arizona',
-    'CA': 'California',
-    'CO': 'Colorado',
-    'CT': 'Connecticut',
-    'DE': 'Delaware',
-    'FL': 'Florida',
-    'GA': 'Georgia',
-    'HI': 'Hawaii',
-    'IA': 'Iowa',
-    'ID': 'Idaho',
-    'IL': 'Illinois',
-    'IN': 'Indiana',
-    'KS': 'Kansas',
-    'KY': 'Kentucky',
-    'LA': 'Louisiana',
-    'MA': 'Massachusetts',
-    'MD': 'Maryland',
-    'ME': 'Maine',
-    'MI': 'Michigan',
-    'MN': 'Minnesota',
-    'MO': 'Missouri',
-    'MS': 'Mississippi',
-    'MT': 'Montana',
-    'NC': 'North Carolina',
-    'ND': 'North Dakota',
-    'NE': 'Nebraska',
-    'NH': 'New Hampshire',
-    'NJ': 'New Jersey',
-    'NM': 'New Mexico',
-    'NV': 'Nevada',
-    'NY': 'New York',
-    'OH': 'Ohio',
-    'OK': 'Oklahoma',
-    'OR': 'Oregon',
-    'PA': 'Pennsylvania',
-    'RI': 'Rhode Island',
-    'SC': 'South Carolina',
-    'SD': 'South Dakota',
-    'TN': 'Tennessee',
-    'TX': 'Texas',
-    'UT': 'Utah',
-    'VA': 'Virginia',
-    'VT': 'Vermont',
-    'WA': 'Washington',
-    'WI': 'Wisconsin',
-    'WV': 'West Virginia',
-    'WY': 'Wyoming'
-}
+states = [
+    'Alaska',
+    'Alabama',
+    'Arkansas',
+    'Arizona',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Iowa',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Massachusetts',
+    'Maryland',
+    'Maine',
+    'Michigan',
+    'Minnesota',
+    'Missouri',
+    'Mississippi',
+    'Montana',
+    'North Carolina',
+    'North Dakota',
+    'Nebraska',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'Nevada',
+    'New York',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Virginia',
+    'Vermont',
+    'Washington',
+    'Wisconsin',
+    'West Virginia',
+    'Wyoming'
+]
 
 cities = [
     "Montgomery", "Janeau", "Pheonix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover",
@@ -120,12 +120,16 @@ while len(people) < 20:
     street_num = random.randint(0, 19)
     streets = street[street_num]
 
-    name = " ".join(first_name, last_name)
+    zip_num = random.randint(0, 19)
+    zip_c = zip_list[zip_num]
 
-    # people[first_name] = 
+    phone_num = random.randint(0, 19)
+    phone_number = phone[phone_num]
 
 
+    people[first_name] = [last_name, address, streets, city, state, zip_c, phone_num]
 
-print(people)
 
+for key, value in people.items():
+        print(key, ': ', value)
 #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
